a595fe0c959380a1321502c7e67049003604bc04
Habibillah <habibillah@gmail.com>
2023-07-12 21:16:57 +0700

    https://github.com/iorad/iorad-extension/issues/2467 | remove moodle internal

--- db/uninstall.php
+++ db/uninstall.php
@@ -22,8 +22,6 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Add the iorad button to the Atto toolbar config if the media plugin is enabled.
  *
